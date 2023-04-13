8d7de0595096dec16bcb4da8ac2c5b17b98ed114
miguelfiais <miguel.fiais10@gmail.com>
2023-04-13 15:02:02 -0300

    feat: add edit product

--- src/components/AdminProducts/index.jsx
+++ src/components/AdminProducts/index.jsx
@@ -1,10 +1,37 @@
+import { useState } from 'react';
 import { IoMdTrash } from 'react-icons/io';
 import { MdModeEdit } from 'react-icons/md';
+import { toast } from 'react-toastify';
 import { useProduct } from '../../hooks/ProductContext';
-import { Container, Offer } from './styles';
+import { api } from '../../services/api';
+import { formartPrice } from '../../utils/utils';
+import { Container, ContainerForm, FormEdit, Offer } from './styles';
 
 const AdminProducts = () => {
   const { products, deleteProduct } = useProduct();
+  const [isOpen, setIsOpen] = useState(false);
+
+  const [productName, setProductName] = useState('');
+  const [productPrice, setProductPrice] = useState('');
+  const [productId, setProductId] = useState('');
+  const [productOffer, setProductOffer] = useState(false);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await toast.promise(
+      api.put(`/products/${productId}`, {
+        name: productName,
+        price: productPrice,
+        offer: productOffer,
+      }),
+      {
+        pending: 'Alterando produto...',
+        success: 'Produto alterado com sucesso',
+        error: 'Falha ao alterar o produto',
+      }
+    );
+    setIsOpen(false);
+  };
 
   return (
     <Container>
@@ -21,15 +48,53 @@ const AdminProducts = () => {
             <div key={product.id} className="tr-body">
               <img src={`http://localhost:3000/image/${product.path}`} alt="" />
               <p>{product.name}</p>
-              <p>{product.price}</p>
-              <Offer isOffer={product.offer} />
+              <p>{formartPrice(product.price)}</p>
+              <Offer isOffer={product.offer}>{product.offer ? 'V' : 'X'}</Offer>
               <div className="delete-and-edit">
-                <MdModeEdit />
+                <MdModeEdit
+                  onClick={() => {
+                    setProductName(product.name);
+                    setProductPrice(product.price);
+                    setProductOffer(product.offer);
+                    setProductId(product.id);
+                    setIsOpen(true);
+                  }}
+                />
                 <IoMdTrash onClick={() => deleteProduct(product.id)} />
               </div>
             </div>
           ))}
       </div>
+      <ContainerForm isOpen={isOpen}>
+        <FormEdit onSubmit={handleSubmit}>
+          <span onClick={() => setIsOpen(false)}>X</span>
+          <label>
+            Nome:
+            <input
+              type="text"
+              value={productName}
+              onChange={(e) => setProductName(e.target.value)}
+            />
+          </label>
+          <label>
+            Preço:
+            <input
+              type="number"
+              value={productPrice}
+              onChange={(e) => setProductPrice(e.target.value)}
+            />
+          </label>
+          <label className="label-checkbox">
+            Oferta:
+            <input
+              type="checkbox"
+              checked={productOffer}
+              onChange={() => setProductOffer(!productOffer)}
+            />
+          </label>
+          <button type="submit">Alterar produto</button>
+        </FormEdit>
+      </ContainerForm>
     </Container>
   );
 };
--- src/components/AdminProducts/styles.js
+++ src/components/AdminProducts/styles.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
+  position: relative;
   padding: 36px;
   background: #f2f2f2;
   .table-head,
@@ -43,6 +44,65 @@ export const Container = styled.div`
 export const Offer = styled.span`
   width: 24px;
   height: 24px;
-  background-color: ${(props) => (props.isOffer ? '#0f0' : '#f00')};
+  background-color: ${(props) => (props.isOffer ? '	#008000' : '#f00')};
   border-radius: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-weight: 500;
+  color: #fff;
+`;
+export const ContainerForm = styled.div`
+  display: ${(props) => (props.isOpen ? 'flex' : 'none')};
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 260px;
+  right: 0;
+  bottom: 0;
+  background: rgba(255, 255, 255, 0.4);
+  backdrop-filter: blur(4px);
+`;
+export const FormEdit = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: max-content;
+  gap: 16px;
+  padding: 24px;
+  border-radius: 8px;
+  background: #a499a6;
+  position: relative;
+  span {
+    position: absolute;
+    top: 1rem;
+    right: 1rem;
+    color: #fff;
+    cursor: pointer;
+    font-size: 16px;
+    font-weight: 500;
+  }
+  label {
+    display: flex;
+    flex-direction: column;
+    font-size: 14px;
+    gap: 4px;
+  }
+  input,
+  > button {
+    width: 200px;
+    height: 36px;
+    border-radius: 8px;
+    border: none;
+  }
+  input {
+    padding-left: 8px;
+  }
+  .label-checkbox {
+    flex-direction: row;
+    input {
+      width: 20px;
+      height: 20px;
+    }
+  }
 `;
